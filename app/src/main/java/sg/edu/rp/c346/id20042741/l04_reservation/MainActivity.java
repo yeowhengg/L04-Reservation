2dfaed38340d1952f626d3a80e7ba4a96e93b45a
Yeow Heng <20042741@myrp.edu.sg>
2022-05-13 15:52:53 +0800

    basic requirements done

--- app/src/main/java/sg/edu/rp/c346/id20042741/l04_reservation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20042741/l04_reservation/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.ToggleButton;
 
 import org.w3c.dom.Text;
 
@@ -20,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
     EditText nameTxt;
     EditText phoneNoTxt;
     EditText groupSizeTxt;
-    Button btnDateTime;
+    Button btnDate;
+    Button btnTime;
     Button btnCfm;
     Button btnReset;
     DatePicker datePicker;
@@ -28,17 +30,23 @@ public class MainActivity extends AppCompatActivity {
     CheckBox cbSmoking;
     TextView datetimeOutput;
     TextView finalOutput;
+    int datecount;
+    int timecount;
+    String datetimeformat;
+    String date;
+    String time;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         nameTxt = findViewById(R.id.txtEditName);
         phoneNoTxt = findViewById(R.id.txtEditNumber);
         groupSizeTxt = findViewById(R.id.txtEditGroupSize);
-        btnDateTime = findViewById(R.id.btnDateTime);
+        btnDate = findViewById(R.id.btnDate);
+        btnTime = findViewById(R.id.btnTime);
         btnCfm = findViewById(R.id.btnCfm);
         btnReset = findViewById(R.id.btnReset);
         datePicker = findViewById(R.id.dateId);
@@ -46,6 +54,52 @@ public class MainActivity extends AppCompatActivity {
         cbSmoking = findViewById(R.id.cbSmoke);
         datetimeOutput = findViewById(R.id.displayDateTime);
         finalOutput = findViewById(R.id.outputDisplay);
+        datecount = 0;
+        timecount = 0;
+        date = "";
+        time = "";
+
+        btnDate.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                if(btnDate.isPressed() && datecount == 0){
+                    datePicker.setVisibility(View.VISIBLE);
+                    datecount++;
+                }
+                else{
+                    datePicker.setVisibility(View.GONE);
+                    datecount = 0;
+                    date = String.format("%d/%d/%d", datePicker.getDayOfMonth(), datePicker.getMonth() + 1, datePicker.getYear());
+                    datetimeformat = String.format("Date: %s | Time: %s",date,time);
+                }
+
+                if(!date.isEmpty()){
+                    datetimeOutput.setText(datetimeformat);
+                }
+            }
+        });
+
+        btnTime.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(btnTime.isPressed() && timecount == 0){
+                    timePicker.setVisibility(View.VISIBLE);
+                    timecount++;
+                }
+                else{
+                    timePicker.setVisibility(View.GONE);
+                    timecount = 0;
+                    time = String.format("%d-%02d", timePicker.getCurrentHour(), timePicker.getCurrentMinute());
+                    datetimeOutput.setText(datetimeformat);
+                    datetimeformat = String.format("Date: %s | Time: %s",date,time);
+                }
+
+                if(!time.isEmpty()){
+                    datetimeOutput.setText(datetimeformat);
+                }
+            }
+        });
 
         btnCfm.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -54,11 +108,11 @@ public class MainActivity extends AppCompatActivity {
                 String getPhone = phoneNoTxt.getText().toString();
                 String getGrpSize = groupSizeTxt.getText().toString();
 
-                if(!inputValidation(getName, getPhone, getGrpSize)){
+                if (!inputValidation(getName, getPhone, getGrpSize)) {
                     finalOutput.setText("Please enter the inputs correctly");
+                } else {
+                    finalOutput.setText(String.format("Reservation booked! Your details are: \n Name: %s \n Number: %s \n"));
                 }
-
-
             }
         });
     }
@@ -66,9 +120,9 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean inputValidation(String name, String phone, String grpSize){
         String namePattern = "^([a-zA-Z]+\\s)*[a-zA-Z]+$";
-        String phonePattern = "[8|9](0-9){7}";
+        String phonePattern = "(8|9)[0-9]{7}";
         String grpPattern = "[0-9]";
 
-        return Pattern.matches(namePattern,name) || Pattern.matches(phonePattern,phone) || Pattern.matches(grpPattern, grpSize);
+        return Pattern.matches(namePattern,name) && Pattern.matches(phonePattern,phone) && Pattern.matches(grpPattern, grpSize);
     }
 }
